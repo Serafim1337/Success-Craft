2dd5fdb9e91deb1bfe351f1a896bbc80343e7878
Serafim <7620226@gmail.com>
2022-06-24 02:02:21 +0300

    feat: stage 3 task 3 done without last point

--- SuccessMotors/force-app/main/default/lwc/batchSchedulerLwc/batchSchedulerLwc.js
+++ SuccessMotors/force-app/main/default/lwc/batchSchedulerLwc/batchSchedulerLwc.js
@@ -2,38 +2,73 @@ import { LightningElement,track,api,wire } from 'lwc';
 
 import scheduleBatch from '@salesforce/apex/BatchSchedulerLwcController.scheduleBatch';
 import abortBatch from '@salesforce/apex/BatchSchedulerLwcController.abortBatch';
+import runBatch from '@salesforce/apex/BatchSchedulerLwcController.runBatch';
+
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class BatchSchedulerLwc extends LightningElement {
 
   batchName = 'ContactsBatch';
   schedulerName = 'ScheduleContactsBatch';
   jobName = 'ScheduleJob'
-  scheduleDate = '0 35 17 * * ?';
+  
 
   @track view = true;
 
   jobId;
 
-  handleRunButton(event) {
+  handleRunButton(e){
 
+    runBatch({batchName: this.batchName}).then(()=>{
+
+      const event = new ShowToastEvent({
+        title: 'Success',
+        message: 'Batch executed',
+        variant: 'success',
+        mode: 'dismissable',
+    });
+    this.dispatchEvent(event);
+
+    }).catch(()=>{
+
+    });
+
+    
   }
 
-  handleScheduleButton(event) {
-      scheduleBatch().then(()=>{
+  handleScheduleButton(e) {
+
+      scheduleBatch({cronString: this.template.querySelector('.input').value,
+      batchName: this.batchName,
+      schedulerName: this.schedulerName
+      }).then(()=>{
        
+        const event = new ShowToastEvent({
+          title: 'Success',
+          message: 'Batch scheduled',
+          variant: 'success',
+          mode: 'dismissable',
+      });
+      this.dispatchEvent(event);
+
       }).catch(()=>{
   
       });
+      
       this.view = !this.view;
   }
 
-  handleAbortButton(event) {
+  handleAbortButton(e) {
 
-    const jobInfo = {
-      name: this.jobName,
-    };
+    abortBatch({jobName: this.jobName}).then(()=>{
 
-    abortBatch({name: this.jobName}).then(()=>{
+      const event = new ShowToastEvent({
+        title: 'Success',
+        message: 'Batch aborted',
+        variant: 'success',
+        mode: 'dismissable',
+    });
+    this.dispatchEvent(event);
 
     }).catch(()=>{
 
